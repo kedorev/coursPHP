682bd129cbfbeb87a7b978df3edeed92d01d1813
kedorev <kedorev@gmail.com>
2015-06-16 15:38:20 +0200

    test

---
+++ Class/Database.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kedoPortable
+ * Date: 16/06/2015
+ * Time: 12:41
+ */
+
+class Database {
+
+    const passeword = "";
+    const identifiant = "";
+
+
+
+}
