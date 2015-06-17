1b134100c01a38816e4554283afea4424d44a5af
Benjamin <benjamin.juarez.garcia@gmail.com>
2015-06-17 11:02:47 +0200

    page article

--- Article.php
+++ Article.php
@@ -8,6 +8,46 @@
     <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
 </head>
 <body>
+<article>
+    <div id="titreArticle">
+
+    </div>
+    <div id="corpsArticle">
+
+    </div>
+</article>
+<article>
+    <div id="titreArticle">
+
+    </div>
+    <div id="corpsArticle">
+
+    </div>
+</article>
+<article>
+    <div id="titreArticle">
+
+    </div>
+    <div id="corpsArticle">
+
+    </div>
+</article>
+<article>
+    <div id="titreArticle">
+
+    </div>
+    <div id="corpsArticle">
+
+    </div>
+</article>
+<article>
+    <div id="titreArticle">
+
+    </div>
+    <div id="corpsArticle">
+
+    </div>
+</article>
 
 
 
