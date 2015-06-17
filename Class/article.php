8863ccbf80ff44dc5078e3c088073d401d00bf14
kedorev <kedorev@gmail.com>
2015-06-17 10:50:58 +0200

    Class Article
    Ajout des variables
      - id_article
      - body-article
      - array_media_article
     Ajout des fonctions :
      - constructeur
    
      Class User
    
      Ajout des variables
        - isLoged
        - isAdmin
        - identifiant
       Ajout des fonctions :
        - constructeur
        - login
        - isAdmin

--- Class/article.php
+++ Class/article.php
@@ -6,6 +6,32 @@
  * Time: 12:51
  */
 
-class Article {
+class Article
+{
+
+    /*
+     * Identidiant de l'article
+     */
+    var $id_Article;
+
+    /*
+     * Corps de l'article
+     */
+    var $body_Article;
+
+    /*
+     * Tableau des médias associés à l'article ( images, video ... )
+     */
+    var $array_media_article;
+
+
+
+    function __construct($array_media_article, $id_Article, $body_Article)
+    {
+        $this->array_media_article = $array_media_article;
+        $this->id_Article = $id_Article;
+        $this->body_Article = $body_Article;
+    }
+
 
 }
--- Class/user.php
+++ Class/user.php
@@ -6,6 +6,93 @@
  * Time: 12:52
  */
 
-class User {
+class User
+{
+    /**
+     * Variable permettant de savoir si l'utilisateur est enregistre
+     */
+    var $isLoged;
+
+    /*
+     * Variable permettant de savoir si l'utilisateur est un administrateur
+     */
+    var $isAdmin;
+
+    /*
+     *  Identifiant de l'utilisateur
+     */
+    var $identifiant;
+
+
+    function __construct($identifiant, $password)
+    {
+        $this->identifiant = $identifiant;
+        if($this::login($identifiant,$password))
+        {
+            $this->isLoged = 1;
+            if (!$this::isAdmin($identifiant))
+            {
+                $this->isAdmin = 0;
+            }
+            else
+            {
+                $this->isAdmin = 1;
+            }
+        }
+        else
+        {
+            $this->isLoged = 0;
+            $this->isAdmin = 0;
+        }
+    }
+
+
+    /*
+    *  Retourne si l'utilisateur a saisie les informations adéquates pour se connecter.
+    *
+    * @var $identifiant, $password
+    * @acces public
+    * @return boolean
+    */
+    function login($identifiant,$password)
+    {
+        $pdo = Database::getInstance();
+        $requete = $pdo->prepare("SELECT count(*) FROM login l WHERE log = ? AND mdp = ?");
+        $requete->exec(array($identifiant,$password));
+        $nb_res = $requete->fetchColumn();
+
+        if ($nb_res == 1)
+        {
+            return true;
+        }// ok
+        else // pas ok
+        {
+            return false;
+        }
+    }
+
+    /*
+     *  Retourne si l'utilisateur à les droits d'administration
+     *
+     * @var $identifiant
+     * @acces public
+     * @return boolean
+     */
+    function isAdmin($identifiant)
+    {
+        $pdo = Database::getInstance();
+        $requete = $pdo->prepare("SELECT admin FROM login l WHERE log = ?");
+        $requete->exec(array($identifiant));
+        $nb_res = $requete->fetchColumn();
+
+        if ($nb_res['admin'] == 1)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
 
 }
