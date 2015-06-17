ae7c5b11dfeb51ab36bca40c47c4ef3efe7d537f
kedorev <kedorev@gmail.com>
2015-06-17 11:54:40 +0200

    Add Ajax folder and 2 page for import Add JQuery
    Add include.php for all header include

---
+++ AJAX/newAccount.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kedoPortable
+ * Date: 17/06/2015
+ * Time: 11:39
+ */
--- Article.php
+++ Article.php
@@ -3,9 +3,10 @@
 <head>
     <title>Article</title>
     <!-- Personal Css -->
-    <link rel="stylesheet" href="css/bootstrap.min.css" />
-    <link rel="stylesheet" href="css/bootstrap-theme.css" />
-    <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+    <?php
+        include 'include.php';
+    ?>
+
 </head>
 <body>
 <article>
