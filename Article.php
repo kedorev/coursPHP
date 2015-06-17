7980d2839078e2e717523196741f099e06f0d7f5
Benjamin <benjamin.juarez.garcia@gmail.com>
2015-06-17 10:26:35 +0200

    developpement d'un echo

---
+++ Article.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+    <title>Article</title>
+    <!-- Personal Css -->
+    <link rel="stylesheet" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" href="css/bootstrap-theme.css" />
+    <link rel="stylesheet" href="css/bootstrap-theme.min.css" />
+</head>
+<body>
+
+
+</body>
+</html>
