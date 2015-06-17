e095da549c3553c77aa736ba0be7a272c641a0a3
kedorev <kedorev@gmail.com>
2015-06-17 09:25:44 +0200

    mise a jour class

--- Class/Database.php
+++ Class/Database.php
@@ -6,11 +6,55 @@
  * Time: 12:41
  */
 
-class Database {
+class Database extends PDO
+{
 
-    const passeword = "";
-    const identifiant = "";
+    /**
+     * Instance de la classe SPDO
+     *
+     * @var $PDOInstance
+     * @access private
+     */
+    private static $PDOInstance = null;
 
 
+    /**
+     * surcharge de la fonction clone pour eviter le clonage et ainsi avoid le singleton
+     *
+     *
+     * @access private
+     */
+    private function __clone()
+    {
 
+    }
+
+
+    /**
+     * Définition des constantes
+     *
+     *
+     * @access private
+     */
+    const host = "mysql:host=maximelede1.mysql.db;";
+    const nameDB = "maximelede1";
+    const username = "maximelede1";
+    const password = "M9Ua35wm";
+
+
+     function __construct()
+    {
+        if(self::$PDOInstance == null)
+        {
+            try
+            {
+                self::$PDOInstance = new PDO(hote, username, password);
+            }
+            catch (PDOException $e)
+            {
+                die("PDO CONNECTION ERROR: " . $e->getMessage() . "<br/>");
+            }
+        }
+        return self::$PDOInstance;
+    }
 }
---
+++ Class/article.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kedoPortable
+ * Date: 16/06/2015
+ * Time: 12:51
+ */
+
+class Article {
+
+}
