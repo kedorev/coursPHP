26dc6da54b083b2c6caa4a671fee1d680d2f265e
Benjamin <benjamin.juarez.garcia@gmail.com>
2015-08-11 14:34:08 +0200

    block

---
+++ fortuneclicker.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html>
+<head>
+    <title>Article</title>
+    <!-- Personal Css -->
+    <?php
+    include 'Include/Header.php';
+    ?>
+
+</head>
+<body>
+
+<div class="blog-masthead">
+    <div class="container">
+        <nav class="blog-nav">
+            <a class="blog-nav-item active" href="#">Home</a>
+            <a class="blog-nav-item" href="#">New features</a>
+            <a class="blog-nav-item" href="#">Press</a>
+            <a class="blog-nav-item" href="#">New hires</a>
+            <a class="blog-nav-item" href="#">About</a>
+        </nav>
+    </div>
+</div>
+
+<div class="container">
+
+    <div class="row">
+
+        <div class="col-sm-8 blog-main">
+
+            <div class="blog-post">
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+
+            </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+
+            </div><!-- /.blog-post -->
+
+            <nav>
+                <ul class="pager">
+                    <li><a href="#">Previous</a></li>
+                    <li><a href="#">Next</a></li>
+                </ul>
+            </nav>
+
+        </div><!-- /.blog-main -->
+
+    </div><!-- /.row -->
+
+</div><!-- /.container -->
+
+</body>
+</html>
