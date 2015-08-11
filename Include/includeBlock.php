951a75c589ae50c5b4448dc606d0e3d81b88be6a
Benjamin <benjamin.juarez.garcia@gmail.com>
2015-08-11 16:39:20 +0200

    block

--- Include/includeBlock.php
+++ Include/includeBlock.php
@@ -9,9 +9,4 @@
 include_once "Block/blockcentre.php";
 include_once "Block/blockdroit.php";
 include_once "Block/blockgauche.php";
-include_once "Block/farmer1.php";
-include_once "Block/farmer2.php";
-include_once "Block/farmer3.php";
-include_once "Block/farmer4.php";
-include_once "Block/pepite.php";
-include_once "Block/store.php";
+?>
